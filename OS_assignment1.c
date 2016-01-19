fbe37794e1e7b8fbd456af820643d8c5423aec28
Israel Hill <idh@case.edu>
2016-01-18 22:57:50 -0500

    first commit

---
+++ OS_assignment1.c
@@ -0,0 +1,37 @@
+#define _XOPEN_SOURCE // required for cuserid to work
+
+// includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <time.h>
+
+// char* cuserid_wrapper() {
+//     char* val = cuserid(NULL);
+//     if (val == NULL) {
+//         perror("cuserid");
+//         exit(errno);
+//     } else {
+//         return val;
+//     }
+// }
+
+int main() {
+  int pid = getpid();
+  int parentPid = getppid();
+  char hostName[1024];
+  //char* userId = cuserid_wrapper();
+  char userId[1024];
+
+  gethostname(hostName, 1024);
+  cuserid(userId);
+
+  printf("Main PID: %d\n", pid);
+  printf("Parent PID: %d\n", parentPid);
+  printf("Hostname: %s\n", hostName);
+  printf("User ID: %s\n", cuserid);
+}
