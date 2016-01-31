c4ce3af56cf67a99517ee4254ab7f9e004082a0b
Israel Hill <idh@case.edu>
2016-01-30 19:35:40 -0500

    fixed fork stuff with sleep

--- os_as1.c
+++ os_as1.c
@@ -10,21 +10,21 @@
 #include <sys/resource.h>
 #include <time.h>
 
-char* cuserid_wrapper() {
-  char* val = cuserid(NULL);
-  if (val == NULL) {
-    perror("cuserid");
-    exit(errno);
-  } else {
-    return val;
-  }
-}
+// char* cuserid_wrapper() {
+//   char* val = cuserid(NULL);
+//   if (val == NULL) {
+//     perror("cuserid");
+//     exit(errno);
+//   } else {
+//     return val;
+//   }
+// }
 
 void printParentInfo() {
   int pid = getpid();
   int parentPid = getppid();
   char hostName[1024];
-  char* userId = cuserid_wrapper();
+  // char* userId = cuserid_wrapper();
   time_t currenttime;
   time(&currenttime);
   char wd[1024];
@@ -33,7 +33,7 @@ void printParentInfo() {
   printf("Main PID: %d\n", pid);
   printf("Parent PID: %d\n", parentPid);
   printf("Hostname: %s\n", hostName);
-  printf("User ID: %s\n", userId);
+  // printf("User ID: %s\n", userId);
   printf("Current Time: %s\n", ctime(&currenttime));
   printf("Working directory %s\n", getcwd(wd, 1024));
 }
@@ -45,25 +45,31 @@ int main() {
   printParentInfo();
 
   child_a = fork();
+
   if(child_a < 0) {
     printf("Error\n");
     exit(1);
   }
   else if(child_a == 0) {
+    //child 1
     printf("I am child 1 with pid: %d\n", getpid());
     exit(0);
   }
   else {
+    // parent
     child_b = fork();
     if(child_b < 0) {
+      //child 2
       printf("Error\n");
     }
     else if(child_b == 0) {
+      sleep(2);
       printf("I am child 2 with pid: %d\n", getpid());
       exit(0);
     }
 
     wait(NULL);
+    sleep(3);
     printf("Children finished\n");
     return 1;
   }
