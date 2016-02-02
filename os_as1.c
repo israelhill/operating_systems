83a3f274784fbab4682d73e49da8b65d6107935c
Israel Hill <idh@case.edu>
2016-02-01 20:13:27 -0500

    sleep() timing is working

--- os_as1.c
+++ os_as1.c
@@ -39,70 +39,101 @@ void printParentInfo() {
   printf("Working directory: %s\n", getcwd(wd, 1024));
 }
 
-void parent_procedure() {
-
+void print_child(char* caller) {
+	fprintf(stdout, "I am child %s, my PID is %d. My Parent is %d.\n", caller, getpid(), getppid());
+	fflush(stdout);
 }
 
-void decreaseWhaleBy(int x) {
-	char *env_var = getenv("WHALE");
+void print_whale(char* caller) {
+  char* env_var;
+  env_var = getenv("WHALE");
+  printf("%s: WHALE is %s\n", caller, env_var);
+}
+
+void decreaseWhaleBy(int x, char* caller) {
+	char* env_var = getenv("WHALE");
 	int new_env_var;
 
 	sscanf(env_var, "%d", &new_env_var);
-  printf("CURRENT VARIABLE: %s\n", env_var);
 	new_env_var = (int) new_env_var - x;
 
   char prefix[] = "WHALE=";
   char buffer[40];
   sprintf(buffer, "%s%d", prefix, new_env_var);
-  printf("NEW ENV VSRISBLE: %d\n", new_env_var);
 
 	int ret_val = putenv(buffer);
-  printf("ENV_VAR is %s\n", getenv("WHALE"));
+
 	if(ret_val < 0) {
 		fprintf(stderr, "ERROR setting environment variable");
 		_exit(errno);
 	}
+  else {
+    fprintf(stdout, "%s: WHALE is %s\n", caller, getenv("WHALE"));
+  	fflush(stdout);
+  }
 }
 
+void parent_procedure() {
+  char* caller = "P0";
 
+  sleep(1);
+  print_whale(caller); //print whale 7
+
+  sleep(3);
+  decreaseWhaleBy(3, caller); // whale 4
+
+  sleep(3);
+  decreaseWhaleBy(3, caller); //whale 1
+}
+
+void child_a_procedure() {
+  char* caller = "C1";
+
+  sleep(1);
+  print_child(caller);
+
+  sleep(2);
+  decreaseWhaleBy(1, caller);
+
+  sleep(3);
+  decreaseWhaleBy(3, caller);
+
+  exit(0);
+}
+
+void child_b_procedure() {
+  char* caller = "C2";
+
+  sleep(1);
+  print_child(caller);
+
+  sleep(3);
+  decreaseWhaleBy(2, caller);
+
+  sleep(3);
+  decreaseWhaleBy(3, caller);
+
+  exit(0);
+}
 
 int main() {
-  int child_a, child_b;
+  pid_t child;
 
   putenv("WHALE=7");
   printParentInfo();
 
-  child_a = fork();
+  child = fork();
+  if(child == 0) {
+    child_a_procedure();
+  }
 
-  if(child_a < 0) {
-    printf("Error\n");
-    exit(1);
+  child = fork();
+  if(child == 0) {
+    child_b_procedure();
   }
-  else if(child_a == 0) {
-    //child 1
-    printf("I am child 1 with pid: %d\n", getpid());
-    exit(0);
-  }
-  else {
-    // parent
-    child_b = fork();
-    if(child_b < 0) {
-      //child 2
-      printf("Error\n");
-    }
-    else if(child_b == 0) {
-      sleep(2);
-      printf("I am child 2 with pid: %d\n", getpid());
-      exit(0);
-    }
 
-    wait(NULL);
-    sleep(3);
-    printf("Children finished\n");
-    char* env_var;
-    env_var = getenv("WHALE");
-    printf("WHALE is %s\n", env_var);
-    decreaseWhaleBy(3);
-    return 1;
-  }
+  parent_procedure();
+  wait(NULL);
+  wait(NULL);
+  return 1;
 }
