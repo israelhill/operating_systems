2e4ba480151e0ddc9714299b56344b746f83dd03
Israel Hill <idh@case.edu>
2016-02-10 16:16:34 -0500

    things are being sent to reducers, but they are reading randomness for infinite amount of time

--- assignment2/as2.c
+++ assignment2/as2.c
@@ -12,9 +12,25 @@
 int mapper_pipes[2][2];
 int reducer_pipes[26][2];
 char* lines[4] = {};
+char counts[LETTERS] = {0};
+int mapperPids[4];
+int reducerPids[26];
+
+void close_reducer_pipes() {
+  for(int i = 0; i < 26; i++) {
+    close(reducer_pipes[i][WRITE_END]);
+  }
+}
+
+void printCounts() {
+  for(int i = 0; i < LETTERS; i++) {
+    printf("count %c: %d\n", i + ALPHA_OFFSET, counts[i]);
+  }
+}
 
 void mapperWork(int mapperPipe) {
   char read_msg[BUFFER_SIZE];
+  int current_pipe;
 
   // read the line
   close(mapper_pipes[mapperPipe][WRITE_END]);
@@ -24,33 +40,34 @@ void mapperWork(int mapperPipe) {
 
   // send characters to the correct reducer
   for(int i = 0; i < strlen(read_msg); i++){
+      current_pipe = i;
       // if it is a lowercase letter, send to correct pipe
       if(read_msg[i] >= ALPHA_OFFSET && read_msg[i] < ALPHA_OFFSET + LETTERS){
         int p = read_msg[i] - ALPHA_OFFSET;
         char letter = read_msg[i];
-        printf("Pipe array index: %d. Character: %c\n", p, letter);
-        printf("Length of character: %lu\n", strlen(&letter));
 
-        close(reducer_pipes[0][READ_END]);
-        write(reducer_pipes[0][WRITE_END], &letter, 1);
-        //close(reducer_pipes[0][WRITE_END]);
+        //printf("Sending: [%c]\n", letter);
 
+        close(reducer_pipes[p][READ_END]);
+        write(reducer_pipes[p][WRITE_END], &letter, 1);
       }
   }
 
-  close(reducer_pipes[0][WRITE_END]);
+  close_reducer_pipes();
   exit(EXIT_SUCCESS);
 }
 
 void reducerWork(int reducerPipe) {
   char buf;
-  printf("Reducer pid: %d, reading from array index: %d\n", getpid(), reducerPipe);
+
+  printf("Reducer Here!!");
 
   sleep(1);
   close(reducer_pipes[reducerPipe][WRITE_END]);
 
-  while(read(reducer_pipes[0][READ_END], &buf, 1) == 1) {
-    printf("REDUCER Char: %c\n", buf);
+  while(read(reducer_pipes[reducerPipe][READ_END], &buf, 1) > 0) {
+    printf("REDUCER index: %d, REDUCER Char: %c\n", reducerPipe, buf);
+    counts[reducerPipe]++;
     sleep(1);
   }
 
@@ -59,7 +76,7 @@ void reducerWork(int reducerPipe) {
 }
 
 void doParent() {
-  for(int i = 0; i < 1; i++) {
+  for(int i = 0; i < 4; i++) {
     close(mapper_pipes[i][READ_END]);
     write(mapper_pipes[i][WRITE_END], lines[i], strlen(lines[i])+1);
     //write(mapper_pipes[i][WRITE_END], "a", strlen("a")+1);
@@ -70,8 +87,6 @@ void doParent() {
 int main() {
   char read_msg[BUFFER_SIZE];
   char buffer[BUFFER_SIZE];
-  pid_t mapper;
-  pid_t reducer;
   FILE *input_file = fopen("input.txt", "r");
 
   // write lines to an array
@@ -90,7 +105,7 @@ int main() {
   }
 
   // create array of reducer pipes
-  for(int i = 0; i < 1; i++) {
+  for(int i = 0; i < 26; i++) {
     if(pipe(reducer_pipes[i]) == -1) {
       perror("ERROR creating pipe!");
       exit(-1);
@@ -98,7 +113,7 @@ int main() {
   }
 
   // create mapper processes
-  for(int i = 0; i < 1; i++) {
+  for(int i = 0; i < 4; i++) {
     pid_t child = fork();
 
     if(child < 0) {
@@ -106,13 +121,18 @@ int main() {
       exit(-1);
     }
     else if(child == 0) { // mapper process
+      printf("calling mapper work\n");
       int mapperPipe = i;
+      //mapperPids[i] = getpid();
       mapperWork(mapperPipe);
+    }
+    else { // parent
+
     }
   }
 
   // create reducer processes
-  for(int i = 0; i < 1; i++) {
+  for(int i = 0; i < 26; i++) {
     pid_t child = fork();
 
     if(child < 0) {
@@ -121,12 +141,15 @@ int main() {
     }
     else if(child == 0) { // reducer process
       int reducerPipe = i;
+      printf("Reducer pid: %d\n", getpid());
+      //reducerPids[i] = getpid();
       reducerWork(reducerPipe);
     }
   }
 
   doParent();
   wait(NULL);
+  //printCounts();
 
   return 1;
 }
